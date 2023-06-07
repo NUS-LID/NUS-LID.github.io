4060d856e8579dcb4d634af020630d8eb1dc8b19
1989Ryan <ziruiz@comp.nus.edu.sg>
2023-06-07 14:57:57 +0800

    update

--- _cite/plugins/google-scholar.py
+++ _cite/plugins/google-scholar.py
@@ -21,6 +21,14 @@ def main(entry):
         "num": 100,  # max allowed
     }
 
+
+    params_ = {
+        "engine": "google_scholar_author",
+        "api_key": "901617e93b784781781d4f365f0fa28d6e2afbd98e5f933ab6f9427a86cf907c",
+        "start": 100,  # max allowed
+        "num": 100,  # max allowed
+    }
+
     # get id from entry
     _id = get_safe(entry, "gsid", "")
     if not _id:
@@ -31,7 +39,10 @@ def main(entry):
     @cache.memoize(name=__file__, expire=1 * (60 * 60 * 24))
     def query(_id):
         params["author_id"] = _id
-        return get_safe(GoogleSearch(params).get_dict(), "articles", [])
+        params_["author_id"] = _id
+        dict_1 = GoogleSearch(params).get_dict()
+        dict_2 = GoogleSearch(params_).get_dict()
+        return get_safe(dict_1, "articles", []) + get_safe(dict_2, "articles", [])
 
     response = query(_id)
 
